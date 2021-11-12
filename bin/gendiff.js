3af73b9730e0e0fedb665416844c7ec6ee1081c9
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-11-12 19:26:23 +0400

    Add changes

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+
+ program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
+
+program.parse();
