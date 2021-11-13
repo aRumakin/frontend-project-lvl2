f89f35e38abb6f6e122169585a5391e495f455db
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-11-13 15:09:06 +0400

    Fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
-import { genDiff } from '../src/index.js'
+import { genDiff } from '../src/index.js';
 
 const program = new Command();
 
- program
+program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
