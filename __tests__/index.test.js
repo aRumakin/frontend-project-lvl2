75d87a9dccb400632585634c981cae53f2eefee1
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-11-16 15:36:56 +0400

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { expect } from '@jest/globals';
+import yaml from 'js-yaml';
 import { genDiff, getFile } from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,6 +10,8 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const jsonParser = (file) => JSON.parse(readFile(file));
+const yamlLoader = (file) => yaml.load(readFile(file));
 
 test('Checking type of result', () => {
   const result1 = JSON.parse(readFile('file1.json'));
@@ -17,13 +20,24 @@ test('Checking type of result', () => {
   expect(typeof result2).toEqual('object');
 });
 
-test('Checking getFile', () => {
-  expect(typeof getFile(getFixturePath('file1.json'))).toEqual(typeof readFile('file1.json'));
-  expect(typeof getFile(getFixturePath('file1.json'))).toEqual(typeof readFile('file2.json'));
+test('Checking getFile with JSON', () => {
+  expect(typeof getFile(getFixturePath('file1.json'))).toEqual(typeof jsonParser('file1.json'));
+  expect(typeof getFile(getFixturePath('file2.json'))).toEqual(typeof jsonParser('file2.json'));
 });
 
-test('Checking the result', () => {
-  const expectDiff = genDiff(readFile('file1.json'), readFile('file2.json'));
+test('Checking getFile with YAML', () => {
+  expect(typeof getFile(getFixturePath('file3.yaml'))).toEqual(typeof yamlLoader('file3.yaml'));
+  expect(typeof getFile(getFixturePath('file4.yaml'))).toEqual(typeof yamlLoader('file4.yaml'));
+});
+
+test('Checking the result with JSON', () => {
+  const expectDiff = genDiff(jsonParser('file1.json'), jsonParser('file2.json'));
   expect(expectDiff).toEqual(readFile('file1-2diff.txt'));
+  expect(typeof expectDiff).toEqual('string');
+});
+
+test('Checking the result with YAML', () => {
+  const expectDiff = genDiff(yamlLoader('file3.yaml'), yamlLoader('file4.yaml'));
+  expect(expectDiff).toEqual(readFile('file3-4diff.txt'));
   expect(typeof expectDiff).toEqual('string');
 });
