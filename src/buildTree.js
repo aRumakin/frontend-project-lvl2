9582fcd66a7eb733358bb4460e9b2b2bba475c8b
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-11-24 16:37:42 +0400

    Update buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -6,21 +6,19 @@ const buildTree = (file1, file2) => {
   return sortedKeys.map((key) => {
     const value1 = file1[key];
     const value2 = file2[key];
+    let result = { type: 'same', key, val: value1 };
     if (!_.has(file1, key)) {
-      return { type: 'add', key, val: value2 };
-    }
-    if (!_.has(file2, key)) {
-      return { type: 'remove', key, val: value1 };
-    }
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { type: 'recursion', key, children: buildTree(value1, value2) };
-    }
-    if (!_.isEqual(value1, value2)) {
-      return {
+      result = { type: 'add', key, val: value2 };
+    } else if (!_.has(file2, key)) {
+      result = { type: 'remove', key, val: value1 };
+    } else if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      result = { type: 'recursion', key, children: buildTree(value1, value2) };
+    } else if (!_.isEqual(value1, value2)) {
+      result = {
         type: 'updated', key, val1: value1, val2: value2,
       };
     }
-    return { type: 'same', key, val: value1 };
+    return result;
   });
 };
 
