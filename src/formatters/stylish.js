b984bedcd803f3c54f6930049a1afe729b936a5f
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-11-24 16:38:04 +0400

    Update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,43 +1,39 @@
-const getIndentation = (depth, space = 4) => ' '.repeat(depth + space);
+const getIndentation = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
 
-const stringify = (someEntity, spaceCount) => {
-  const iter = (current, depth) => {
-    if (typeof current !== 'object') {
-      return `${current}`;
-    }
-    if (current === '') { return ''; }
-    if (current === null) { return null; }
-    const lines = Object
-      .entries(current)
-      .map(([key, value]) => `${getIndentation(depth + 4)}${key}: ${iter(value, depth + 4)}`);
-    return [
-      '{',
-      ...lines,
-      `${getIndentation(depth)}}`,
-    ].join('\n');
-  };
-
-  return iter(someEntity, spaceCount);
+const stringify = (data, depth) => {
+  if (typeof data !== 'object') {
+    return `${data}`;
+  }
+  if (data === null) { return null; }
+  const lines = Object
+    .entries(data)
+    .map(([key, value]) => `${getIndentation(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
+  return [
+    '{',
+    ...lines,
+    `${getIndentation(depth)}  }`,
+  ].join('\n');
 };
 
 function stylish(data) {
   const iter = (tree, depth) => tree.map((node) => {
+    const getValue = (value, sign) => `${getIndentation(depth)}${sign} ${node.key}: ${stringify(value, depth)}\n`;
     switch (node.type) {
       case 'add':
-        return `${getIndentation(depth - 2)}+ ${node.key}: ${stringify(node.val, depth)}\n`;
+        return getValue(node.val, '+');
       case 'remove':
-        return `${getIndentation(depth - 2)}- ${node.key}: ${stringify(node.val, depth)}\n`;
+        return getValue(node.val, '-');
       case 'same':
-        return `${getIndentation(depth - 2)}  ${node.key}: ${stringify(node.val, depth)}\n`;
+        return getValue(node.val, ' ');
       case 'updated':
-        return `${getIndentation(depth - 2)}- ${node.key}: ${stringify(node.val1, depth)}\n${getIndentation(depth - 2)}+ ${node.key}: ${stringify(node.val2, depth)}\n`;
+        return `${getValue(node.val1, '-')}${getValue(node.val2, '+')}`;
       case 'recursion':
-        return `${getIndentation(depth)}${node.key}: {\n${iter(node.children, depth + 4).join('')}${getIndentation(depth)}}\n`;
+        return `${getIndentation(depth)}  ${node.key}: {\n${iter(node.children, depth + 1).join('')}${getIndentation(depth)}  }\n`;
       default:
         throw new Error(`These type is not exists: ${node.type}`);
     }
   });
-  return `{\n${iter(data, 0).join('')}}`;
+  return `{\n${iter(data, 1).join('')}}`;
 }
 
 export default stylish;
