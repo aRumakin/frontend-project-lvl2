eef709cb02fe330210b943ad8094aa982c35d13f
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-12-05 13:25:33 +0400

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import fs from 'fs';
 import path from 'path';
-import parseFile from './parsers.js';
-import stylish from './formatters/stylish.js';
-import plain from './formatters/plain.js';
+import { formatter, parseFile } from './formatters/index.js';
 import buildTree from './buildTree.js';
 
-const getFile = (filepath) => {
+const getData = (filepath) => {
   const pathToFile = fs.readFileSync(path.resolve(process.cwd(), filepath.trim()), 'utf-8');
-  return parseFile(pathToFile, filepath);
+  const format = path.extname(filepath).slice(1);
+  return parseFile(pathToFile, format);
 };
 
-const formatter = (tree, formatterName) => {
-  switch (formatterName) {
-    case 'stylish':
-      return stylish(tree);
-    case 'plain':
-      return plain(tree);
-    case 'json':
-      return JSON.stringify(tree);
-    default:
-      return `Format ${formatterName} is not supported.`;
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const getTree = buildTree(getData(filepath1), getData(filepath2));
+  let result = '';
+  try {
+    result = formatter(getTree, formatName);
+  } catch (e) {
+    return e.message;
   }
+  return result;
 };
 
-const genDiff = (file1, file2, formatName = 'stylish') => {
-  const getTree = buildTree(file1, file2);
-  return formatter(getTree, formatName);
-};
-
-export { genDiff, getFile };
+export default genDiff;
