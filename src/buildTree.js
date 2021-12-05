54f5ca64a21d0224d45d2f33bbff494100a774ee
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-12-05 13:24:52 +0400

    Update buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,15 +1,15 @@
 import _ from 'lodash';
 
-const buildTree = (file1, file2) => {
-  const keys = Object.keys({ ...file1, ...file2 });
+const buildTree = (data1, data2) => {
+  const keys = Object.keys({ ...data1, ...data2 });
   const sortedKeys = _.sortBy(keys);
   return sortedKeys.map((key) => {
-    const value1 = file1[key];
-    const value2 = file2[key];
+    const value1 = data1[key];
+    const value2 = data2[key];
     let result = { type: 'same', key, val: value1 };
-    if (!_.has(file1, key)) {
+    if (!_.has(data1, key)) {
       result = { type: 'add', key, val: value2 };
-    } else if (!_.has(file2, key)) {
+    } else if (!_.has(data2, key)) {
       result = { type: 'remove', key, val: value1 };
     } else if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
       result = { type: 'recursion', key, children: buildTree(value1, value2) };
